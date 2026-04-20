08d1237f53dff38f7589157ffa817382a51e4803
Pratik Pujari <pratikpujari1000@gmail.com>
2026-04-19 19:27:34 -0600

    refactor(ratelimiter): remove unused rate limiting methods and prefixes fix(deps): update golang-jwt/jwt to v5.3.1

--- services/user-service/api/v1/rate_limiter/config.go
+++ services/user-service/api/v1/rate_limiter/config.go
@@ -3,17 +3,6 @@ package ratelimiter
 import "time"
 
 const (
-	RLLoginIPPrefix        = "rl:login:ip:"
-	RLLoginUserPrefix      = "rl:login:user:"
-	RLRegisterIPPrefix     = "rl:register:ip:"
-	RLPwdForgotIPPrefix    = "rl:pwd_forgot:ip:"
-	RLPwdForgotEmailPrefix = "rl:pwd_forgot:email:"
-	RLPwdResetIPPrefix     = "rl:pwd_reset:ip:"
-	RLResendVerifyPrefix   = "rl:resend_verify:"
-	RLOtpSendPhonePrefix   = "rl:otp_send:phone:"
-	RLRefreshDevicePrefix  = "rl:refresh:device:"
-	RL2FAUserPrefix        = "rl:2fa:user:"
-
 	RLProfileUpdatePrefix = "rl:profile_update:"
 	RLContactAddPrefix    = "rl:contact_add:"
 	RLBlockPrefix         = "rl:block:"
@@ -26,29 +15,13 @@ const (
 	HeartbeatPrefix   = "heartbeat:"
 	DeviceCountPrefix = "device_count:"
 
-	LockLoginPrefix    = "lock:login:"
-	LockRegisterPrefix = "lock:register:"
-	LockContactPrefix  = "lock:contact:"
-	LockBlockPrefix    = "lock:block:"
-
-	IPBlockedPrefix     = "ip:blocked:"
-	AccountLockedPrefix = "account:locked:"
-	PwdResetSentPrefix  = "pwd_reset:sent:"
+	LockContactPrefix = "lock:contact:"
+	LockBlockPrefix   = "lock:block:"
 
 	PrivacyCachePrefix = "privacy:"
 )
 
 const (
-	TTLLoginWindow    = 15 * time.Minute
-	TTLRegisterIP     = time.Hour
-	TTLPwdForgotIP    = time.Hour
-	TTLPwdForgotEmail = time.Hour
-	TTLPwdResetIP     = time.Hour
-	TTLResendVerify   = time.Hour
-	TTLOtpSendPhone   = time.Hour
-	TTLRefreshDevice  = time.Hour
-	TTL2FAUser        = 15 * time.Minute
-
 	TTLProfileUpdate = time.Hour
 	TTLContactAdd    = time.Hour
 	TTLBlockAction   = time.Hour
@@ -61,15 +34,8 @@ const (
 	TTLHeartbeat   = 35 * time.Second
 	TTLDeviceCount = 35 * time.Second
 
-	TTLLockLogin    = 10 * time.Second
-	TTLLockRegister = 10 * time.Second
-	TTLLockContact  = 10 * time.Second
-	TTLLockBlock    = 10 * time.Second
-
-	TTLIPBlocked             = 24 * time.Hour
-	TTLAccountLocked         = 30 * time.Minute
-	TTLAccountLockedExtended = 24 * time.Hour
-	TTLPwdResetSent          = 5 * time.Minute
+	TTLLockContact = 10 * time.Second
+	TTLLockBlock   = 10 * time.Second
 
 	TTLPrivacyCache = 2 * time.Minute
 )
